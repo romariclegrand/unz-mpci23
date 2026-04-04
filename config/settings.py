f4bdb44aaecbd124d4a652f25ebb281d8d0bff24
romariclegrand <romaricyelkouni1@gmail.com>
2026-04-04 16:29:47 +0000

    Fix CSRF trusted origins

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+CSRF_TRUSTED_ORIGINS = ['https://unz-mpci23-production.up.railway.app']
