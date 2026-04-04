3c5bbee3c362ee44a719b379de64ca576a6d4968
romariclegrand <romaricyelkouni1@gmail.com>
2026-04-04 16:57:03 +0000

    Fix superuser creation

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(username='romaric').exists() or User.objects.create_superuser('romaric', 'romaricyelkouni1@gmail.com', 'unzmpci2025')" | python manage.py shell
+python manage.py createsuperuser --no-input || true
