e86c0060f7cc7cac8b47b1a5006bf3a03de6da8a
romariclegrand <romaricyelkouni1@gmail.com>
2026-04-07 00:37:52 +0000

    Create superuser in build

--- build.sh
+++ build.sh
@@ -3,4 +3,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input || true
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='romaric').exists():
+    User.objects.create_superuser('romaric', 'romaricyelkouni1@gmail.com', 'unzmpci2025')
+    print('Superuser created')
+else:
+    print('Superuser exists')
+"
