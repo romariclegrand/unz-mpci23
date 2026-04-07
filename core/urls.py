8e774cbcbcd9943849d990a18e90e14df30a7b3b
romariclegrand <romaricyelkouni1@gmail.com>
2026-04-07 20:00:04 +0000

    "Fix videos url"

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('actualites/', views.actualites, name='actualites'),
     path('bibliotheque/', views.bibliotheque, name='bibliotheque'),
     path('images/', views.images, name='images'),
+    path('videos/', views.videos, name='videos'),
     path('apropos/', views.apropos, name='apropos'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
